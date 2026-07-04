e8d28fb233c78ff9a935b4d2cf94e8578658822c
namanydv <namanofficial91@gmail.com>
2026-07-05 00:23:54 +0530

    Add missing newline for improved code readability

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { googleAuth, resetPassword, sendOtp, signIn, signOut, signUp, verifyOtp } from "../controllers/auth.controllers.js"
 
+
 const authRouter=express.Router()
 
 authRouter.post("/signup",signUp)
