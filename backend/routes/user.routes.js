710029c1ad62b7ef4ff623c24c7a165b8449fa32
namanydv <namanofficial91@gmail.com>
2026-07-08 07:49:07 +0530

    Remove unnecessary newline for cleaner code structure

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,6 @@ import express from "express"
 import { getCurrentUser, updateUserLocation } from "../controllers/user.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 
-
 const userRouter=express.Router()
 
 userRouter.get("/current",isAuth,getCurrentUser)
